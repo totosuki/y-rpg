e12d45b7b86adf6fe3c4e4d65696ff8bb94a7cf3
toripppppy <Lundy11@icloud.com>
2024-01-12 12:30:49 +0900

    ダメージ計算処理を作成

--- Assets/Scripts/Battle/BattleManager.cs
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -5,11 +5,25 @@ using Fungus;
 
 public class BattleManager : MonoBehaviour
 {
+    private class Entity
+    {
+        public int hp;
+
+        public Entity(int hp)
+        {
+            this.hp = hp;
+        }
+    }
+
+
     [SerializeField] private GameObject player;
 
     [SerializeField] private AttackManager attackManager;
 
     private Enemy enemy;
+
+    private Entity playerEntity;
+    private Entity enemyEntity;
     
     // 子オブジェクトからの参照用としても使う
     public Flowchart flowchart;
@@ -17,7 +31,8 @@ public class BattleManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerEntity = new Entity(100);
+        enemyEntity = new Entity(enemy.lv * 10);
     }
 
     // Update is called once per frame
@@ -35,6 +50,28 @@ public class BattleManager : MonoBehaviour
         SetBattleConfigByEnemy();
     }
 
+    public void PlayerTurnEnd()
+    {
+        bool isCritical = attackManager.IsCritical();
+        // ATK ハードコードしてます
+        float damage = CalculateDamage(3, isCritical);
+
+        // この攻撃で相手を倒せるかどうか
+        if (enemyEntity.hp - damage <= 0)
+        {
+            print("enemy lose");
+        }
+        else
+        {
+            applyDamage(enemyEntity, (int)damage);
+        }
+    }
+
+    public void PlayerAttack()
+    {
+        attackManager.DoAttack();
+    }
+
     public void EndBattle()
     {
         player.GetComponent<Collider2D>().enabled = true;
@@ -50,5 +87,22 @@ public class BattleManager : MonoBehaviour
     {
         attackManager.animationCurve = enemy.animationCurve;
         attackManager.duration = enemy.duration;
+    }
+
+    float CalculateDamage(float atk, bool isCritical)
+    {
+        float criticalRate = 2.0f;
+        float damage = atk * 10;
+        if (isCritical) {
+            damage *= criticalRate;
+        }
+
+        return Mathf.Floor(damage);
+    }
+
+    void applyDamage(Entity target, int damage)
+    {
+        print($"{damage} damage to {target}");
+        target.hp -= damage;
     }
 }
