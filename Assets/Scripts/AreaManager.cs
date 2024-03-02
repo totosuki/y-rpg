b617cc707246bc0af5a0c90a4c5e00a79df97afc
toripppppy <Lundy11@icloud.com>
2024-03-02 12:37:39 +0900

    記憶ロジックのコメントアウト行を削除

--- Assets/Scripts/AreaManager.cs
+++ Assets/Scripts/AreaManager.cs
@@ -1,43 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using Fungus;
 using UnityEngine;
 
 public class AreaManager : MonoBehaviour
 {
     public AreaInfo areaInfo;
-
-    // 多分今の仕様的に記憶はできないから遺産
-
-    // public GameManager gameManager;
-
-    // [Tooltip("一定時間は同じエリア移動を再通知しない")]
-    // [SerializeField]private int rememberTime;
-
-    // private List<string> rememberList = new List<string>();
-
-    // public void RememberArea(string areaName)
-    // {
-    //     // 数秒間記憶する
-    //     rememberList.Add(areaName);
-    //     StartCoroutine(RemoveAreaFromRememberList(areaName));
-    // }
-
-    // IEnumerator RemoveAreaFromRememberList(string areaName)
-    // {
-    //     yield return new WaitForSeconds(rememberTime);
-    //     rememberList.Remove(areaName);
-    // }
-
-    // public void ShowAreaInfo(string areaName, string subtitle)
-    // {
-    //     if (!rememberList.Contains(areaName))
-    //     {
-    //         areaInfo.SetInfo(areaName, subtitle);
-    //         areaInfo.Show();
-    //     }
-    // }
 
     public void ShowAreaInfo(string areaName, string subtitle)
     {
