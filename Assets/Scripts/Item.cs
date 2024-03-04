ccb87b1162b948b7d2b515cc5aea87067265b815
totosuki <e231kari@gmail.com>
2024-03-04 10:28:41 +0900

    item関連のスクリプトを作成

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum ItemType { None, Item, Coin }
+
+    [SerializeField]
+    private ItemManager itemManager;
+
+    [Header("アイテムの設定")]
+    [SerializeField]
+    private ItemType itemType = ItemType.None;
+    
+    [Header("アイテムのID")]
+    public int itemId;
+
+    [Header("アイテムの名前")]
+    public string itemName;
+
+    [Header("アイテムの説明")]
+    [TextArea(3, 10)]
+    public string description;
+
+    private void ItemTriggerEnter() 
+    {
+        itemManager.AddCollectItemBit(itemId);
+    }
+
+    private void CoinTriggerEnter() 
+    {
+        Debug.Log("[ItemScript] コインを取った");
+    }
+
+    public void CollectItem() 
+    {
+        switch (itemType) {
+            case ItemType.Item:
+                ItemTriggerEnter();
+                break;
+            case ItemType.Coin:
+                CoinTriggerEnter();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player") {
+            CollectItem();
+            gameObject.SetActive(false);
+        }
+    }
+}
