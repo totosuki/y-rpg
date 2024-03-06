d6b1163c0b0800304fd2a7b27625aad38e5a6294
totosuki <e231kari@gmail.com>
2024-03-06 19:56:07 +0900

    ミッションをターンに合わせて表示する処理を実装

--- Assets/Scripts/MissionManager.cs
+++ Assets/Scripts/MissionManager.cs
@@ -1,8 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MissionManager : MonoBehaviour
 {
+    public TextMeshProUGUI missionTitle;
+    public TextMeshProUGUI missionDescription;
+
+
     [System.Serializable]
     private class Mission
     {
@@ -14,4 +19,17 @@ public class MissionManager : MonoBehaviour
     [SerializeField]
     [Header("ミッション一覧")]
     private List<Mission> missions;
+
+    public void ShowMission(int turn)
+    {
+        foreach (Mission mission in missions)
+        {
+            if (mission.turn == turn)
+            {
+                missionTitle.text = mission.title;
+                missionDescription.text = mission.description;
+                break;
+            }
+        }
+    }
 }
