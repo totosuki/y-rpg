c48a64123743c5e5784d9e3add6fd5f59418f669
toripppppy <Lundy11@icloud.com>
2024-03-07 21:03:42 +0900

    CanInteract()からメッセージが空でないという条件を削除

--- Assets/Scripts/MessageTrigger.cs
+++ Assets/Scripts/MessageTrigger.cs
@@ -40,12 +40,6 @@ public class MessageTrigger : InteractionTrigger
         onInteract.AddListener(InvokeBlock);
     }
 
-    public override bool CanInteract()
-    {
-        // メッセージが空で無いことを条件に加える
-        return base.CanInteract() && message != "";
-    }
-
     // Fungusのブロックを実行する
     IEnumerator SendMessage(Action callback)
     {
@@ -91,6 +85,12 @@ public class MessageTrigger : InteractionTrigger
 
     public void InvokeBlock()
     {
+        // メッセージが空なら実行しない
+        if (message == "")
+        {
+            return;
+        }
+        
         playerController.DisableCanMove();
 
         StartCoroutine(SendMessage(() => {
