28aeb86f9d84417ab7708ec2b9c65e3a243f0da8
toripppppy <Lundy11@icloud.com>
2024-03-07 22:06:13 +0900

    MissionManagerをマージ

---
+++ Assets/Scripts/MissionManager.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class MissionManager : MonoBehaviour
+{
+    public TextMeshProUGUI missionTitle;
+    public TextMeshProUGUI missionDescription;
+
+
+    [System.Serializable]
+    private class Mission
+    {
+        public int turn;
+        public string title;
+        public string description;
+    }
+
+    [SerializeField]
+    [Header("ミッション一覧")]
+    private List<Mission> missions;
+    
+    private GameManager gameManager;
+    private CanvasGroup canvasGroup;
+
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        canvasGroup = GetComponent<CanvasGroup>();
+
+        gameManager.onTurnUpdate.AddListener(ListenTurnUpdate);
+    }
+
+    public void ShowMission(int turn)
+    {
+        bool isChange = false;
+
+        foreach (Mission mission in missions)
+        {
+            if (mission.turn == turn)
+            {
+                missionTitle.text = mission.title;
+                missionDescription.text = mission.description;
+                isChange = true;
+                break;
+            }
+        }
+
+        if (!isChange)
+        {
+            missionTitle.text = "";
+            missionDescription.text = "";
+        }
+
+        StartCoroutine(FadeIn());
+    }
+
+    // ターン更新に反応する処理
+    public void ListenTurnUpdate()
+    {
+        int turn = gameManager.GetCurrentTurn();
+        ShowMission(turn);
+    }
+
+    public IEnumerator FadeIn()
+    {
+        float t = 0f;
+        canvasGroup.alpha = t;
+
+        while (t < 1f)
+        {
+            t += 0.005f;
+            canvasGroup.alpha = t;
+            transform.localPosition += new Vector3(t/25, 0);
+
+            yield return null;
+        }
+    }
+}
